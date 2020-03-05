e80f89d2a2d1a417ddc7b6d5062c79f35434efdc
Станіслав Євгенович Киян <stanislav_kijan@ia.ua>
2020-03-05 02:30:44 -0500

    Add virtual moving character

--- VRNavigation/Assets/Scripts/PlayerScript.cs
+++ VRNavigation/Assets/Scripts/PlayerScript.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float speed;
+    public float mouseSensitivity;
+
+    private void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float translationZ = Input.GetAxis("Vertical");
+        float translationX = Input.GetAxis("Horizontal");
+        var direction = transform.TransformDirection(new Vector3(translationX, 0, translationZ));
+        direction = new Vector3(direction.x, 0, direction.z).normalized * speed * Time.deltaTime;
+        transform.position += direction;
+
+        if (!Input.GetKey(KeyCode.LeftControl))
+        {
+            Cursor.visible = false;
+            float h = mouseSensitivity * Input.GetAxis("Mouse X");
+            float v = mouseSensitivity * Input.GetAxis("Mouse Y");
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x - v, transform.rotation.eulerAngles.y + h, 0);
+        }
+        else
+        {
+            Cursor.visible = true;
+        }
     }
+
+
 }
